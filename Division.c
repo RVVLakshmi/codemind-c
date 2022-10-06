393af83dabf88b68d02c4b87b1b55b34bb9f30af
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2022-10-06 12:51:03 +0530

    #include<stdio.h> int main(){   int num1, num2, quotient;
      //Asking for input
      scanf("%d", &num1);
      scanf("%d", &num2);
      
      //Computing quotient
      quotient = num1 / num2;
      printf("%d", quotient);
      return 0;
    }

---
+++ Division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int num1, num2, quotient;
+  
+  //Asking for input
+  scanf("%d", &num1);
+  scanf("%d", &num2);
+  
+  //Computing quotient
+  quotient = num1 / num2;
+  printf("%d", quotient);
+  return 0;
+}
+
