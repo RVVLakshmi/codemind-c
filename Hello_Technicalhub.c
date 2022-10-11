a46c52ac118b51fe00d130c6257b898aa5dafb8c
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2022-10-11 21:53:41 +0530

    #include <stdio.h>
    int main()
    {
      
        // Declare string variable
        // as character array
        char str[50];
      
        // --- String ---
        scanf("%[^
    ]
    s", str);
      
        // Output the String
        printf("Hello Technicalhub
    ");
        printf("%s", str);
      
        return 0;
    }

---
+++ Hello_Technicalhub.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+  
+int main()
+{
+  
+    // Declare string variable
+    // as character array
+    char str[50];
+  
+    // --- String ---
+    scanf("%[^
+]
+s", str);
+  
+    // Output the String
+    printf("Hello Technicalhub
+");
+    printf("%s", str);
+  
+    return 0;
+}
